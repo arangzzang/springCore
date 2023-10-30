ddd1e0d346cb297faf6408f0b7f83c32656707c5
aranag <dkfkd1198@gamil.com>
2023-10-30 17:25:12 +0900

    회원,주문,할인 도메인 개발 / OCP,DIP 규정 위배되어 소준수할수있도록 소스 수정

---
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,29 @@
+package hello.core.member;
+
+import hello.core.AppConfig;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MemberServiceTest {
+
+    MemberService memberService;
+    @BeforeEach // 테스트실행전 무조건 실행되는 빈
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
+
+    @Test
+    void join(){
+        //given
+        Member member = new Member(1L,"memberA",Grade.VPI);
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+        //then
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+}
