c38a51b21487b992b066207b51e37e34676d9008
arang <dkfkd1198@gmail.com>
2023-11-02 10:28:30 +0900

    AppConfig 리팩터링 - 사용영역과 구성영역 구분

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,22 +1,29 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
-import hello.core.member.Member;
-import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
-import hello.core.member.MemoryMemberRepository;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.*;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Configuration;
 
-public class AppConfig {// 앱 전체를 설정하고 구성하는 class
+@Configuration
+public class AppConfig {// 앱전체를 설정하고 구성하는 class
 
     public MemberService memberService(){ // 생성자 주입해서 DIP준수
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
-
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
     public OrderService orderService(){
         return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FixDiscountPolicy());
+                memberRepository(),
+                discountPolicy());
+    }
+    public DiscountPolicy discountPolicy(){
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
