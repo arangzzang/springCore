9d81003d3e6d83904108c0550081367118cb1489
arang <dkfkd1198@gmail.com>
2024-01-23 23:28:38 +0900

    싱글톤패턴 적용

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -26,4 +26,20 @@ public class SingletonTest {
         //memberService != memberService2
         assertThat(memberService).isNotSameAs(memberService2);
     }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest(){
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+
+        singletonService1.login();
+    }
+
+
 }
