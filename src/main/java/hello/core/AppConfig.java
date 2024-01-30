d84d365c592d005f1c7d0c5efcdb7a3dbf233a40
aranag <dkfkd1198@gamil.com>
2024-01-30 17:17:07 +0900

    @Configuration과 싱글톤 File : - AppConfig.java - ConfigurationSingletonTest.java - MemberServiceImpl.java - OrderServiceImpl.java

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,9 +1,11 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
-import hello.core.member.*;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
@@ -14,14 +16,17 @@ public class AppConfig {// 앱전체를 설정하고 구성하는 class
 
     @Bean
     public MemberService memberService(){ // 생성자 주입해서 DIP준수
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
     @Bean
     public MemberRepository memberRepository(){
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
     @Bean
     public OrderService orderService(){
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(
                 memberRepository(),
                 discountPolicy());
