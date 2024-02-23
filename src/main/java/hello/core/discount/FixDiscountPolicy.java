b9e6c79f8407adbed5b08601558477b89df1ae9d
aranag <dkfkd1198@gamil.com>
2024-02-23 17:02:02 +0900

    생성자 주입을 선택해라!
    File
    - FixDiscountPolicy.java
    - OrderServiceImplTest.java
    
    정리
    생성자 주입 방식을 선택하는 이유는 여러가지가 있지만, 프레임워크에 의존하지 않고, 순수한 자바 언어의 특징
    을 잘 살리는 방법이기도 하다.
    기본으로 생성자 주입을 사용하고, 필수 값이 아닌 경우에는 수정자 주입 방식을 옵션으로 부여하면 된다. 생성자
    주입과 수정자 주입을 동시에 사용할 수 있다.
    항상 생성자 주입을 선택해라! 그리고 가끔 옵션이 필요하면 수정자 주입을 선택해라. 필드 주입은 사용하지 않는
    게 좋다.

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -5,7 +5,7 @@ import hello.core.member.Member;
 
 public class FixDiscountPolicy implements DiscountPolicy{
 
-    private int DiscountFixAmount = 1000;//1000원 할인
+    private int DiscountFixAmount = 10000;//1000원 할인
     @Override
     public int discount(Member member, int price) {
         if(member.getGrade() == Grade.VIP){
