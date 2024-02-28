a2aa125b6bcce158ac1e4d3de0416b4f763c560d
aranag <dkfkd1198@gamil.com>
2024-02-28 20:34:05 +0900

    조회한 빈이 모두 필요할 때, List, Map
    File
    - AllBeanTest.java
    - FixDiscountPolicy.java
    
    정리
    1. DiscountService는 Map으로 모든 DiscountPolicy 를 주입받는다.
    이때 fixDiscountPolicy,rateDiscountPolicy 가 주입된다. -> @Component 혹은 빈등록이 되어 있는 컴포넌트
    
    2. discount () 메서드는 discountCode로 "fixDiscountPolicy"가 넘어오면 map에서
       fixDiscountPolicy 스프링 빈을 찾아서 실행한다. 물론 “rateDiscountPolicy”가 넘어오면
       rateDiscountPolicy 스프링 빈을 찾아서 실행한다
    주입 분석
    
    Map<String, DiscountPolicy> : map의 키에 스프링 빈의 이름을 넣어주고, 그 값으로
    DiscountPolicy 타입으로 조회한 모든 스프링 빈을 담아준다.
    
    List<DiscountPolicy> : DiscountPolicy 타입으로 조회한 모든 스프링 빈을 담아준다.
    
    만약 해당하는 타입의 스프링 빈이 없으면, 빈 컬렉션이나 Map을 주입한다.

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class FixDiscountPolicy implements DiscountPolicy{
 
-    private int DiscountFixAmount = 10000;//1000원 할인
+    private int DiscountFixAmount = 1000;//1000원 할인
     @Override
     public int discount(Member member, int price) {
         if(member.getGrade() == Grade.VIP){
