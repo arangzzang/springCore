271ca20653272d012d841669aea50e9c1a2d4b6a
arang <dkfkd1198@gmail.com>
2024-03-08 18:05:13 +0900

    프로토타입 스코프 - 싱글톤 빈과 함께 사용시 Provider로 문제 해결
    File
    - SingletonWithPrototypeTest1.java
    
    issue
    - 스프링부트 3.0 이상일 때는 javax.inject:javax.inject:1 대신 jakarta.inject:jakarta.inject-api:2.0.1를 gradle에 추가헤야함.

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,11 +2,16 @@ package hello.core.scope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import jakarta.inject.Provider;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
+
+//import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -38,25 +43,41 @@ public class SingletonWithPrototypeTest1 {
         int count1 = clientBean1.logic();
         assertThat(count1).isEqualTo(1);
 
+//        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+//        int count2 = clientBean2.logic();
+//        assertThat(count2).isEqualTo(2);
+
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
 
     @Scope("singleton")
+    @RequiredArgsConstructor
     static class ClientBean {
-        private final PrototypeBean prototypeBean; // 생성시점에 주입
-        private final ApplicationContext applicationContext; // 빈등록확인
+//        private final PrototypeBean prototypeBean; // 생성시점에 주입
+//        private final ApplicationContext applicationContext; // 빈등록확인
 
+        /**
+         * 의존관계를 외부에서 주입(DI) 받는게 아니라 이렇게 직접 필요한 의존관계를 찾는 것을 Dependency Lookup (DL) 의존관계 조회(탐색) 이라한다.
+         * 그런데 이렇게 스프링의 애플리케이션 컨텍스트 전체를 주입받게 되면, 스프링 컨테이너에 종속적인 코드가 되고, 단위 테스트도 어려워진다.
+         * 지금 필요한 기능은 지정한 프로토타입 빈을 컨테이너에서 대신 찾아주는 딱! ** DL **  정도의 기능만 제공하는 무언가가 있으면 된다.
+         * */
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean, ApplicationContext applicationContext) {
-            this.prototypeBean = prototypeBean;
-            this.applicationContext = applicationContext;
-            System.out.println("applicationContext = " + applicationContext);
-        }
+        private Provider<PrototypeBean> provider;
+//        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+
+//        @Autowired
+//        public ClientBean(PrototypeBean prototypeBean, ApplicationContext applicationContext) {
+//            this.prototypeBean = prototypeBean;
+//            this.applicationContext = applicationContext;
+//            System.out.println("applicationContext = " + applicationContext);
+//        }
 
         public int logic() {
+//            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            PrototypeBean prototypeBean = provider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
