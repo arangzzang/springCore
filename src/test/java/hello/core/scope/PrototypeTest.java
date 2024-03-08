d42d1a69a9b37b7810e6e7df40cf0c9a03f4d403
arang <dkfkd1198@gmail.com>
2024-03-08 13:47:56 +0900

    빈 스코프란? : 스코프는 번역 그대로 빈이 존재할 수 있는 범위를 뜻한다.
    프로토타입 스코프
    
    File
    - SingletonTest.java
    - PrototypeTest.java
    
    ** 싱글톤 ** : 기본 스코프, 스프링 컨테이너의 시작과 종료까지 유지되는 가장 넓은 범위의 스코프이다.
    
    ** 프로토타입 ** : 스프링 컨테이너는 프로토타입 빈의 생성과 의존관계 주입까지만 관여하고 더는 관리하지 않는 매우 짧은 범위의 스코프이다.
    
    ** 웹 관련 스코프 **
    ** request ** : 웹 요청이 들어오고 나갈때 까지 유지되는 스코프이다.
    ** session ** : 웹 세션이 생성되고 종료될 때 까지 유지되는 스코프이다.
    ** application ** : 웹의 서블릿 컨텍스트와 같은 범위로 유지되는 스코프이다.
    
    prototypeBean 특징
    1. 스프링 컨테이너에 요청할 때 마다 새로 생성됨.
    2. 스프링 컨테이너는 프로토타입 빈의 생성과 의존관계 주입 그리고 초기화까지만 관여한다.
    3. 종료 메서드가 호출되지 않는다.
    4. 그래서 프로토타입 빈은 프로토타입 빈을 조회한 클라이언트가 관리해야한다. 종료메서드에 대한 호출도 클라이언트가 직접 해야함.

---
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,46 @@
+package hello.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+
+        System.out.println("find prototypeBean1");
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        System.out.println("find prototypeBean2");
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+
+        assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+
+
+        ac.close(); // 프로토타입 빈은 스프링 컨테이너가 생성과 의존관계 주입 그리고 초기화 까지만 관여하고, 더는 관리하지 않는다. ( `@PreDestroy'메서드 호출X)
+        // 따라서 직접 호출해줘야함.
+        prototypeBean1.detroy();
+        prototypeBean2.detroy();
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void detroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+
+}
