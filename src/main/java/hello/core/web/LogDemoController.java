8f5b90edccea2cfe13a7376e8f7b3d8680999f8b
arang <dkfkd1198@gmail.com>
2024-05-19 23:30:35 +0900

    스코프와 프록시
    File
    - LogDemoController.java
    - LogDemoService.java
    - logback.xml

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -13,13 +13,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+//    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) throws InterruptedException {
         String requsetURL = request.getRequestURI().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
+//        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requsetURL);
 
         myLogger.log("controller test");
